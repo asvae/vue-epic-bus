e97d3e35eddd8b4dc2528bbc1ee54784b3531eef
Asva <ontrew@gmail.com>
2019-02-25 23:31:24 +0300

    chore: move everything to single file

--- src/BusPlugin.js
+++ src/BusPlugin.js
@@ -1,8 +1,39 @@
-import { Bus } from './Bus'
-
 const defaultOptions = {
   optionGroupName: 'subs',
   broadcastName: '$cast',
+}
+
+export class Bus {
+  constructor () {
+    // Subscribed components - array of closures
+    this.subscribers = new Map
+  }
+
+  on (event, closure, component) {
+    if (Array.isArray(event)) {
+      event.forEach(event => this.on(event, closure, component))
+      return
+    }
+
+    const componentSubscribes = this.subscribers.get(component) || this.subscribeNew(component)
+    componentSubscribes[event] = closure
+  }
+
+  emit (event, ...args) {
+    this.subscribers.forEach((subscribes, component) => {
+      subscribes[event] && subscribes[event].call(component, ...args)
+    })
+  }
+
+  wipeComponentSubscriptions (component) {
+    this.subscribers.delete(component)
+  }
+
+  subscribeNew (component) {
+    const componentSubscribes = {}
+    this.subscribers.set(component, componentSubscribes)
+    return componentSubscribes
+  }
 }
 
 export const BusPlugin = {
